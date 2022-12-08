168e1d7368a7cfb6b3ae7951aba2e204af57945d
Maha1208 <Mahalakshmi.raja@aspiresys.com>
2022-12-08 09:40:08 +0530

    javascr

--- Encapsulation.js
+++ Encapsulation.js
@@ -1,11 +1,8 @@
 class Person {
-
-    name;
-  
-    constructor(name) {
+     name;
+      constructor(name) {
       this.name = name;
     }
-  
     introduceSelf() {
       console.log(`Hi! I'm ${this.name}`);
     }
@@ -16,25 +13,26 @@ class Student extends Person {
 
     #year;
   
-    constructor(name, year) {
+    constructor(name, year) 
+    {
       super(name);
       this.#year = year;
     }
-  
-  
-    introduceSelf() {
+    introduceSelf() 
+    {
       console.log(`Hi! I'm ${this.name}, and I'm in ${this.#year} year.`);
     }
   
-    canStudyArchery() {
-      return this.#year > 1;
+    canStudyArchery() 
+    {
+      return this.#year > 4;
     }
 
   }
   const maha= new Student('maha', 2);
 
-maha.introduceSelf(); // Hi! I'm Summers, and I'm in year 2.
+maha.introduceSelf(); // Hi! I'm maha, and I'm in year 2.
 maha.canStudyArchery(); // true
 
-//summers.#year; //syntax error
+//maha.#year; //syntax error
   
